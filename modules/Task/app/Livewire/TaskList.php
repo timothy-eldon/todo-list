1adde477fd0aacddf4aa417af284bff4d68fc4ee
Nathaniel <egbodonathaniel@yahoo.com>
2025-03-14 16:09:04 +0100

    create task livewire component

---
+++ modules/Task/app/Livewire/TaskList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Task\App\Livewire;
+
+use Livewire\Component;
+
+class TaskList extends Component
+{
+    public function render()
+    {
+        return view('task::livewire.task-list');
+    }
+}
---
+++ modules/Task/resources/views/livewire/task-list.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h3>The <code>TaskList</code> livewire component is loaded from the <code>Task</code> module.</h3>
+</div>
