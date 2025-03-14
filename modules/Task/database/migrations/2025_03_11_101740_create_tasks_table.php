c48f06fd265b521a06dd96957c51678b2515d91e
Nathaniel <egbodonathaniel@yahoo.com>
2025-03-14 15:40:31 +0100

    migrate task table

--- modules/Task/database/migrations/2025_03_11_101740_create_tasks_table.php
+++ modules/Task/database/migrations/2025_03_11_101740_create_tasks_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('title')->unique()->nullable(true);
+            $table->text('description')->nullable();
+            $table->boolean('is_completed')->default(false);
             $table->timestamps();
         });
     }
