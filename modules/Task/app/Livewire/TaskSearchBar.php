1296536ac835a176d57dc95529481218891559b1
Nathaniel <egbodonathaniel@yahoo.com>
2025-03-27 13:40:17 +0100

    created task search bar livewire component

---
+++ modules/Task/app/Livewire/TaskSearchBar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Task\App\Livewire;
+
+use Livewire\Component;
+
+class TaskSearchBar extends Component
+{
+    public function render()
+    {
+        return view('task::livewire.task-search-bar');
+    }
+}
---
+++ modules/Task/resources/views/livewire/task-search-bar.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h3>The <code>TaskSearchBar</code> livewire component is loaded from the <code>Task</code> module.</h3>
+</div>
